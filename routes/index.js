d99c097a03a0fa7a8d33b4de959e1b94b8699e31
Lena Sultanova <sultanova.e.v@yandex.ru>
2023-08-05 11:31:39 +0300

    ПР 13. Исправления после 2 итерации

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
-// const NotFound = require('../errors/NotFound');
+const NotFound = require('../errors/NotFound');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
 router.use((req, res, next) => {
-  next(res.status(404).send('Запрашиваемый ресурс не найден'));
+  next(new NotFound('Запрашиваемый ресурс не найден'));
 });
 
 module.exports = router;
